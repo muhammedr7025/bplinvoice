7fd2d803d0e256ce48909736b80cb796a0714c83
muhammedr7025 <muhammedr7025@gmail.com>
2023-02-25 16:51:03 +0530

    implemented class

---
+++ lib/invoice.dart
@@ -0,0 +1,22 @@
+import 'dart:ffi';
+
+class Invoice {
+  int? noOfPcs;
+  Float? totalWeight;
+  Float? totalValue;
+  String? portOfDischarge;
+  String? finalDestination;
+  String? portOfLoading;
+  String? modeOfShippment;
+  String? senderName;
+  String? senderAddres;
+  int? sendermob;
+  int? senderTel;
+  String? reciverName;
+  String? reciverAddres;
+  int? recivermob;
+  int? reciverTel;
+  String? orgin;
+  String? destination;
+  String? sNo;
+}
